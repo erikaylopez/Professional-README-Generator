cc475fff382e9ca63c089a0f9ec529033cdff6a9
erikaylopez <eylopez8686@gmail.com>
2024-04-23 19:11:02 -0600

    markdown js content added

--- Develop/README.md
+++ Develop/README.md
@@ -7,7 +7,7 @@
 # Description 📝
 The purpose of this README generator is to provide a high quality sample of what a README file should entail so that the intended audience can benefit from reading and learning from the content provided. This README file was created utilizing Node.js.
 
-# Table of Contents 📋
+# Table of Contents 📚
 - [Installations](#installations)
 - [Usage](#usage)
 - [Contributing](#contributing)
@@ -16,11 +16,16 @@ The purpose of this README generator is to provide a high quality sample of what
 - [License](#license)
 
 # Installation 📎
-To install the project dependencies, fork or clone the repository [Professional-README-Generator](https://github.com/erikaylopez/Professional-README-Generator). Open the terminal and navigate to the root directory of the project. 
-
+To install the project dependencies, follow these steps:
+- Fork and clone the repository [Professional-README-Generator](https://github.com/erikaylopez/Professional-README-Generator).
+- Run `npm i` to install dependencies in the local project directory.
 
 
 # Usage 💻
+1. Open integrated terminal.
+2. Run node index.js.
+3. Answer the prompts that follow.
+4. A sampleREADME.md will be generated and stored in the "utils" folder.
 
 
 # License &copy;
@@ -33,7 +38,7 @@ Licensed under the MIT license.
 Developer: Erika Lopez
 
 
-# Tests ✔
+# Tests 
 npm test
 
 
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,20 +1,82 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license) if (license !== "None") {
+    return `![Github License](https://img.shields.io/badge/license-${license}-yellowgreen.svg)`
+  }
+  return ""
+}
+
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+//function renderLicenseLink(license) {
+
+  function renderLicenseLink(license) {
+    if (license !== "None") {
+      return (
+        `* [License](#license-&copy)`
+      )
+    }
+    return ""
+  }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+
+function renderLicenseSection(license) {
+  if (license !== "None") {
+    return (
+   `## License &copy;
+
+    Copyright @ ${license}. All rights reserved.
+
+    Licensed under the ${license} license.`
+    )
+  }
+  return ""
+}
+
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  ${renderLicenseBadge(data.license)}
 
 `;
 }
+
+# Professional-README-Generator
+
+# Description 📝
+${data.description}
+
+# Table of Contents 📚
+[Installation](#installation-📎)
+[Usage](#usage-💻)
+[Contributing](#contributing-✍)
+[Tests](#tests-🧪)
+[questions](#questions-❓)
+${renderLicenseLink(data.license)}
+
+# Installation 📎
+${data.dependencies}
+
+#Usage 💻
+${data.usage}
+
+${renderLicenseSection(data.license)}
+
+#contributing ✍
+${data.contributing}
+
+#Tests 🧪
+${data.tests}
+
+#Questions ❓
+Github: erikaylopez
+Email: eylopez8686@gmail.com
+Phone Number: 915-929-2209
+
 
 module.exports = generateMarkdown;
